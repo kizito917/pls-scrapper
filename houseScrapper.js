f9225262e210f9730128ae6f39b0461dc04afbad
kizito917 <johnkingsley917@gmail.com>
2023-08-18 09:59:19 +0100

    Changes made to houseScrapper file.

--- houseScrapper.js
+++ houseScrapper.js
@@ -9,12 +9,12 @@ require("dotenv").config();
 const callScrapper = async (res) => {
     const browser = await puppeteer.launch({
         headless: "new",
-        /*args: [
+        args: [
           "--disable-setuid-sandbox",
           "--no-sandbox",
           "--single-process",
           "--no-zygote",
-        ],*/
+        ],
         executablePath:
           process.env.NODE_ENV === "production"
             ? process.env.PUPPETEER_EXECUTABLE_PATH
